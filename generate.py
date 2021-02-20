8c20ec831bfe2453a3e69d57b418a459ed5b4de9
ikelockett72 <79371334+ikelockett72@users.noreply.github.com>
2021-02-20 16:22:59 +0000

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,11 @@
+import inflect
+from classes import WordNumber
+engine = inflect.engine()
+makeNumberWord = engine.number_to_words
+
+for i in range(1000):
+    num = makeNumberWord(i)
+    try:
+        exec(f"globals()[num] = WordNumber(num)", globals())
+    except KeyError:
+        print(f"NotImplementedError: The number {num} is not yet implemented. Please keep updating for new numbers")
