940a3cdb3a7e24c0f808456263473a00b242f797
Ike Lockett <ike.lockett@anaplan.com>
2021-02-22 21:45:18 +0000

    more differences than last time

--- generate.py
+++ generate.py
@@ -5,6 +5,13 @@ makeNumberWord = engine.number_to_words
 
 for i in range(1000):
     num = makeNumberWord(i).replace(" ", "").replace("-","")
+    with open("number_list.py", "r") as f:
+        num_list = f.read()
+        exec(num_list)
+        num_convert[num] = str(i)
+    with open("number_list.py", "w") as f:
+        num_convert_str = "num_convert = " + str(num_convert)
+        f.write(num_convert_str)
     try:
         exec(f"globals()[num] = WordNumber(num)", globals())
     except NotImplementedError:
